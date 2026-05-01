c28772d56966d75c4f3897f9785b4e4c803e0860
이채원 <twothousandwon14@naver.com>
2026-05-01 23:12:35 +0900

    대강 뭐할지 정리

---
+++ mainMenu.py
@@ -0,0 +1,8 @@
+
+def MainMenu():
+    while True : 
+        print("\n" + "="*15 + " MENU " + "="*15)
+        print("\n 1. 알림 설정 ")
+        print("\n 2. 꺼짐 설정 ")
+        print("\n 3. 바탕화면 정리 ")
+        print("\n 4. 다운로드 정리 ")
---
+++ shuttingDown.py
@@ -0,0 +1,5 @@
+import sys
+import os
+from PyQt6.QtWidgets import (QApplication, QWidget, QVBoxLayout, 
+                             QLineEdit, QPushButton, QLabel, QHBoxLayout)
+from PyQt6.QtCore import Qt
